91ed2ad31609c463db242fa76f36da7bb068bfad
Samuel <samuel.d.lewis@wmich.edu>
2017-12-07 13:01:01 -0500

    documentation

--- PandaVoipServer.py
+++ PandaVoipServer.py
@@ -6,11 +6,13 @@ import time
 import json
 
 
+# object for voice client information
 class VoiceClient(object):
     def __init__(self, client_id, addr):
         self.client_id = client_id
         self.addr = addr
 
+    # comparitor
     def am_i(self, client_id, addr):
         return self.client_id == client_id and self.addr == addr
 
@@ -23,18 +25,25 @@ class UDPVoiceHandler(socketserver.BaseRequestHandler):
         data = self.request[0]
         socket = self.request[1]
         addr = self.client_address
+        # get the client_id
         client_id = int.from_bytes(data[0:4], byteorder='little')
+        # check if this client_id is allowed
         if not self.server.check_allowed_client(client_id):
             return
+        # add client if new connection
         self.server.add_client_if_new(client_id, addr)
+        # send to all other clients
         for client in self.server.connections:
+            # check if the client is the one who sent the data
             if not client.am_i(client_id, addr):
+                # check if the client is in the voice chat
                 if (client.client_id in self.server.allowed_connections):
                     self.server.socket.sendto(data, client.addr)
                 else:
                     self.server.connections.remove(client)
 
 
+# voice server
 class ThreadedVoiceServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
     def __init__(self, *args, **kwargs):
         super(ThreadedVoiceServer, self).__init__(*args, **kwargs)
@@ -67,25 +76,35 @@ class CommandClient(object):
 
 class TCPCommandHandler(socketserver.BaseRequestHandler):
     def handle(self):
+        # handle their connection until they disconnect
         while True:
             try:
+                # read the data
                 data = self.request.recv(8192).strip()
+                # only care about the non 0 data
                 data = data.split(b'\x00')[0]
                 try:
+                    # try to parse a json
                     request = json.loads(data.decode())
                 except ValueError:
+                    # tell them if it is invalid
                     json_data = {
                         "command": "nack",
                         "message": "invalid json"
                     }
                     self.server.send_data(self.request, json.dumps(json_data))
                     continue
+            # client disconnect
             except ConnectionResetError:
                 self.server.disconnect(client_id)
                 return
+
+            # parse the data
             client_id = request['client_id']
             command = request['command']
             self.server.add_client_if_new(client_id, self.request)
+
+            # establishing a new connection
             if command == "establish":
                 json_data = {
                     "command": "ack",
@@ -94,6 +113,7 @@ class TCPCommandHandler(socketserver.BaseRequestHandler):
                 self.server.send_data(self.request, json.dumps(json_data))
                 self.server.update_chat_clients()
                 self.server.update_voice_clients()
+            # connecting to the voice chat
             elif command == "voice connect":
                 json_data = {
                     "command": "ack",
@@ -103,6 +123,7 @@ class TCPCommandHandler(socketserver.BaseRequestHandler):
                 self.server.send_data(self.request, json.dumps(json_data))
 
                 self.server.update_voice_clients()
+            # disconnecting from voice chat
             elif command == "voice disconnect":
                 json_data = {
                     "command": "ack",
@@ -112,8 +133,10 @@ class TCPCommandHandler(socketserver.BaseRequestHandler):
                 self.server.send_data(self.request, json.dumps(json_data))
 
                 self.server.update_voice_clients()
+            # sending a text message
             elif command == "text message":
                 self.server.text_message(client_id, request)
+            # anything else isn't valid
             else:
                 json_data = {
                     "command": "nack",
@@ -129,11 +152,15 @@ class ThreadedCommandServer(socketserver.ThreadingMixIn, socketserver.TCPServer)
         self.connections = []
         self.voice_server = None
 
+    # wrapper for sending data
     def send_data(self, socket, message):
+        # send the length of the message first
         length = len(message.encode())
         socket.sendall(str(length).zfill(10).encode())
+        # send the actual message
         socket.sendall(message.encode())
 
+    # send the list of chat clients to all users
     def update_chat_clients(self):
         users = [str(c.client_id).zfill(8) for c in self.connections]
         json_data = {
@@ -143,6 +170,7 @@ class ThreadedCommandServer(socketserver.ThreadingMixIn, socketserver.TCPServer)
         for client in self.connections:
             self.send_data(client.socket, json.dumps(json_data))
 
+    # send the list of voice users to all clients
     def update_voice_clients(self):
         users = [str(c).zfill(8) for c in self.voice_server.allowed_connections]
         json_data = {
@@ -170,6 +198,7 @@ class ThreadedCommandServer(socketserver.ThreadingMixIn, socketserver.TCPServer)
         self.update_chat_clients()
         self.voice_disconnect(client_id)
 
+    # relays a chat message to all users
     def text_message(self, client_id, request):
         response = {
             "command": "new_message",
@@ -204,16 +233,19 @@ HOST = 'localhost'
 voice_port = 50038
 command_port = 50039
 
+# start voice server
 voice_server = ThreadedVoiceServer((HOST, voice_port), UDPVoiceHandler)
 voice_server_thread = threading.Thread(target=voice_server.serve_forever)
 voice_server_thread.daemon = True
 voice_server_thread.start()
 
+# start the command server
 command_server = ThreadedCommandServer((HOST, command_port), TCPCommandHandler)
 command_server_thread = threading.Thread(target=command_server.serve_forever)
 command_server_thread.daemon = True
 command_server_thread.start()
 
+# give each server a reference of the other
 voice_server.attach_command_server(command_server)
 command_server.attach_voice_server(voice_server)
 
